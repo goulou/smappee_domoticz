803ae1ffabdef075ef4d7ce2ce0f3ed82382f484
goulou <goulou@(none)>
2016-11-16 20:05:41 +0100

    update box IP

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ USAGE
         if verbose > 0:
             print("Verbose mode on")
     
-        sm = Smappee("192.168.7.20")
+        sm = Smappee("10.42.0.78")
         sm.login_test("admin")
         
         consumption_counter = EnergyCounter("Consumption")
